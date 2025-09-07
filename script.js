279a2b88d121a718ab32ff39191dae64a5bdde9b
Luan Lucino <109971413+LuanLucino@users.noreply.github.com>
2025-09-07 09:03:19 -0300

    Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const intro = document.querySelector(".intro");
+  intro.addEventListener("click", () => {
+    alert("Tenente aprovou, então tá certo!");
+  });
+});
